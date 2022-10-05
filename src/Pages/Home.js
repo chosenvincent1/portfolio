5e530ba6a84acbd0a47a5a038eba38f122ee57da
mrchosen <chosenvincent@gmail.com>
2022-10-05 15:11:22 -0700

    Added more navlink to the Home Page

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -13,16 +13,11 @@ const Home = ()=> {
             <section className="first-home-section">
                 <div className="home-text">
                     <SecondNavbar className='second-navbar' />
-                    {/* <h1 className="logo">chosen</h1> */}
                     <p className="first-p">hi!</p>
                     <p className="second-p">i'm chosen,</p>
                     {/* <p className="third-p">web developer</p> */}
                     <p className="fourth-p">“I’m a Web Developer with a solid background in JavaScript and React. <br /> i'm also a MERN Stack Developer”</p>
-                    {/* <div className="project-completed">
-                        <p>over 207 <span className="plus">+</span></p>
-                        <p>projects <span className="completed">completed</span></p>
-                        <p><span className="percent">100%</span> reliable</p>
-                    </div> */}
+                   
                     <button className="hire-me">Hire me</button>
                     <div className='home-social-media'>
                         <a href='https://www.twitter.com/chosenvincent1'>
@@ -44,21 +39,21 @@ const Home = ()=> {
                 </div>
                 
 
-                {/* <div className="project-completed">
+                <div className="project-completed">
                     <p>over 207 <span className="plus">+</span></p>
                     <p>projects <span className="completed">completed</span></p>
                     <p><span className="percent">100%</span> reliable</p>
-                </div> */}
+                </div>
 
                 <div className="home-image">
                     <div className="home-image-btn">
                         <button className="btn">lets' talk</button>
                     </div>
-                    <img src="/images/html.png" alt="" className="html" />
+                    {/* <img src="/images/html.png" alt="" className="html" />
                     <img src="/images/css.png" alt="" className="css" />
                     <img src="/images/javascript.png" alt="" className="javascript" />
                     <img src="/images/react.png" alt="" className="react" />
-                    <img src="/images/node.png" alt="" className="node" />
+                    <img src="/images/node.png" alt="" className="node" /> */}
                     <img src="/images/developer.png" alt="" className="developer" />
                     {/* <img src="/images/mongodb.png" alt="" className="mongodb" /> */}
                 </div>
